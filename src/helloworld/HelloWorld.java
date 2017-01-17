c81d67cc2d392c3935c795f79419ef30876c2c5c
Luke <Luke@192.168.2.151>
2017-01-17 17:55:42 -0600

    First Commit for lab

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ * @author Luke Hodinka
+ */
+public class HelloWorld 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //Print to the screen
+        System.out.print("Hello World, my GitHub account name is MaddLu\n");
+    } 
+}
