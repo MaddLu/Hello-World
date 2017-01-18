6e52effea14e68a200ccaceea4920b0e478ef2ea
Luke <Luke@192.168.2.151>
2017-01-17 18:00:35 -0600

    Final commit for lab.  Added fun info about the username being my youtube name.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -12,6 +12,6 @@ public class HelloWorld
     {
         //Print to the screen
         System.out.print("Hello World, my GitHub account name is MaddLu\n");
-        System.out.print("MaddLu is also my youtuber name.\n");
+        System.out.print("MaddLu is also my youtuber name.  Just a fun fact.\n");
     } 
 }
